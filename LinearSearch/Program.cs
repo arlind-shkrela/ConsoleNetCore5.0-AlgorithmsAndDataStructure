0a52841c5c296945552d61ed54c44bbda211fcb5
Arlind Shkrela <arlind.shkrela@outlook.com>
2021-07-28 15:09:15 +0200

    change

--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -6,7 +6,7 @@ namespace LinearSearch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World v1!");
         }
     }
 }
